d9ff2cf31c8c01f6b064fb236640db300928ce77
corvinus-f4v5so <bence.vinkovics@stud.uni-corvinus.hu>
2021-09-29 17:51:30 +0200

    Delete function added.

--- UserMaintenance/UserMaintenance/Resource1.Designer.cs
+++ UserMaintenance/UserMaintenance/Resource1.Designer.cs
@@ -70,6 +70,15 @@ namespace UserMaintenance {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Törlés.
+        /// </summary>
+        internal static string delete {
+            get {
+                return ResourceManager.GetString("delete", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Teljes név.
         /// </summary>
         internal static string FullName {
