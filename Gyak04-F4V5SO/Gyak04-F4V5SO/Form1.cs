895c1a7b45a6a3e527704727b57c92eb1a1e4bcd
Bence Vinkovics <bence.vinkovics@stud.uni-corvinus.hu>
2021-10-07 18:52:46 +0200

    CreateExcel function added

---
+++ Gyak04-F4V5SO/Gyak04-F4V5SO/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
+
+namespace Gyak04_F4V5SO
+{
+    public partial class Form1 : Form
+    {
+        List<Flat> Flats;
+        RealEstateEntities context = new RealEstateEntities();
+
+        Excel.Application xlApp;
+        Excel.Workbook xlWB;
+        Excel.Worksheet xlSheet;
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadData();
+            CreateExcel();
+        }
+
+        private void LoadData()
+        {
+            Flats = context.Flats.ToList();
+
+        }
+
+        private void CreateExcel() 
+        { 
+            try
+            {
+                xlApp = new Excel.Application();
+
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+
+                xlSheet = xlWB.ActiveSheet;
+
+                //CreateTable();
+
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex)
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
+
+
+        }
+    }
+}
