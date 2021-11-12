5d657569cce23db92698c4f1c59a14911cb0b859
Bence Vinkovics <bence.vinkovics@stud.uni-corvinus.hu>
2021-11-12 20:46:44 +0100

    Abstraction implemented in Factories

---
+++ week08/week08/Abstractions/IToyFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using week08.Entities;
+
+namespace week08.Abstractions
+{
+    public interface IToyFactory
+    {
+        Toy CreateNew();
+    }
+}
