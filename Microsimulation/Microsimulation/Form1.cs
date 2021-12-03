5e1d3f1cc2acc68a400a02fdd1f0701baadc027f
Bence Vinkovics <bence.vinkovics@stud.uni-corvinus.hu>
2021-12-03 19:28:48 +0100

    Base of simulation done

--- Microsimulation/Microsimulation/Form1.cs
+++ Microsimulation/Microsimulation/Form1.cs
@@ -23,11 +23,30 @@ namespace Microsimulation
         public Form1()
         {
             InitializeComponent();
-            Population = GetPopulation(@"C:\Temp\nép.csv");
+            Population = GetPopulation(@"C:\Temp\nép-teszt.csv");
             BirthProbabilities = GetBirthPorbability(@"C:\Temp\születés.csv");
             DeathProbabilities = GetBDeathPorbability(@"C:\Temp\halál.csv");
 
-            dataGridView1.DataSource = BirthProbabilities;
+            dataGridView1.DataSource = Population;
+
+            for (int year = 2005; year <= 2024; year++)
+            {
+                for (int i = 0; i < Population.Count; i++)
+                {
+
+                }
+
+                int nbrOfMales = (from x in Population
+                                  where x.Gender == Gender.Male && x.IsAlive
+                                  select x).Count();
+
+                int nbrOfFemales = (from x in Population
+                                  where x.Gender == Gender.Female && x.IsAlive
+                                  select x).Count();
+
+                Console.WriteLine(
+                    string.Format("Év:{0} Fiúk:{1} Lányok:{2}", year, nbrOfMales, nbrOfFemales));
+            }
         }
 
         public List<Person> GetPopulation(string csvpath)
